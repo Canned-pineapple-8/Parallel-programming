347f79bad0aa5cc483d1916bf8170e6da8bce3a5
Canned-pineapple-8 <nastentsiya.filimonova@mail.ru>
2026-02-16 16:32:49 +0300

    Add task-3

--- OpenMpPractice.cpp
+++ OpenMpPractice.cpp
@@ -1,12 +1,24 @@
 ﻿#include <iostream>
 #include <omp.h>
+#include <format>
+
 
 int main()
 {
+    int num_threads;
 
-#pragma omp parallel num_threads(4)
+    std::cout << "Enter threads number: ";
+    std::cin >> num_threads;
+
+    if (num_threads < 0)
     {
-        std::cout << "Hello, world!\n";
+        std::cout << "\nThreads number must be non-negative.";
+        exit(-1);
+    }
+
+#pragma omp parallel num_threads(num_threads)
+    {
+        std::cout << std::format("I am thread {} from {} threads!\n", omp_get_thread_num(), omp_get_num_threads());
     }
 
     return 0;
