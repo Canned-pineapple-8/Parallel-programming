bc19367cc2ed4016c0ed550cd7e5d81fc765ff65
Canned-pineapple-8 <nastentsiya.filimonova@mail.ru>
2026-02-16 17:16:19 +0300

    Add task-5 v1

--- OpenMpPractice.cpp
+++ OpenMpPractice.cpp
@@ -5,22 +5,40 @@
 
 int main()
 {
-    int num_threads;
+    int total_sum = 0, N;
 
-    std::cout << "Enter threads number: ";
-    std::cin >> num_threads;
+    std::cout << "Enter N: ";
+    std::cin >> N;
 
-    if (num_threads < 0)
+    if (N < 0)
     {
-        std::cout << "\nThreads number must be non-negative.";
+        std::cout << "N must be non-negative.\n";
         exit(-1);
     }
 
-#pragma omp parallel num_threads(num_threads)
+#pragma omp parallel num_threads(2) reduction(+:total_sum)
     {
         int thread_number = omp_get_thread_num();
-        std::cout << std::format("I am thread {} from {} threads!\n", omp_get_thread_num(), omp_get_num_threads());
+        int start, end;
+        if (thread_number)
+        {
+            start = 0;
+            end = N / 2;
+        }
+        else
+        {
+            start = N / 2 + 1;
+            end = N;
+        }
+
+        for (int i = start; i < end + 1; ++i)
+        {
+            total_sum += i;
+        }
+        std::cout << std::format("[{}]: Sum = {}\n", thread_number, total_sum);
     }
+
+    std::cout << std::format("Sum = {}\n", total_sum);
 
     return 0;
 }
