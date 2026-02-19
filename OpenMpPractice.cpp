39848443b5018e7789ec7bf73bbee5de30814256
Canned-pineapple-8 <nastentsiya.filimonova@mail.ru>
2026-02-19 13:44:14 +0300

    Add task-10

--- OpenMpPractice.cpp
+++ OpenMpPractice.cpp
@@ -3,55 +3,6 @@
 #include <format>
 #include <string>
 
-int read_value(int&);
-
-
-void matrix_free(int** matrix, int n)
-{
-    for (int i = 0; i < n; ++i)
-        delete[] matrix[i];
-
-    delete[] matrix;
-}
-
-
-int** matrix_init(int n)
-{
-    int** matrix = new int* [n];
-
-    for (int i = 0; i < n; ++i)
-        matrix[i] = new int[n];
-
-    return matrix;
-}
-
-int read_matrix(int **a, int n)
-{
-    std::cout << "Enter matrix:\n";
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = 0; j < n; ++j)
-        {
-            if (!read_value(a[i][j])) return 0;
-        }
-    }
-    std::cout << std::endl;
-
-    return 1;
-}
-
-void print_matrix(int** a, int n)
-{
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = 0; j < n; ++j)
-        {
-            std::cout << a[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
-}
-
 int read_value(int& val)
 {
     if (!(std::cin >> val))
@@ -62,7 +13,7 @@ int read_value(int& val)
     return 1;
 }
 
-int read_var(int& var, std::string var_name)
+int read_var(int& var, std::string var_name, int lower_bound = 1, int upper_bound = 10)
 {
     std::cout << std::format("Enter {}: ", var_name);
     if (!read_value(var))
@@ -70,54 +21,46 @@ int read_var(int& var, std::string var_name)
         return 0;
     }
 
-    if (var < 1 || var > 10)
+    if (var < lower_bound || var > upper_bound)
     {
-        std::cout << std::format("Value {} must be between 1 and 10.\n", var_name);
+        std::cout << std::format("Value {} must be between {} and {}.\n", var_name, lower_bound, upper_bound);
         return 0;
     }
     return 1;
 }
 
-void multiply(int** a, int** b, int** c, int n, int threads_num)
+void sections(int threads_num)
 {
-#pragma omp parallel for num_threads(threads_num) schedule(static)
-    for (int i = 0; i < n; ++i)
+#pragma omp parallel num_threads(threads_num) 
     {
-        for (int j = 0; j < n; ++j)
+#pragma omp sections
         {
-            double sum = 0.0;
+#pragma omp section
+            {
+                std::cout << std::format("[{}]: came in section {}\n", omp_get_thread_num(), 1);
+            }
 
-            for (int k = 0; k < n; ++k)
-                sum += a[i][k] * b[k][j];
+#pragma omp section
+            {
+                std::cout << std::format("[{}]: came in section {}\n", omp_get_thread_num(), 2);
+            }
 
-            c[i][j] = sum;
+#pragma omp section
+            {
+                std::cout << std::format("[{}]: came in section {}\n", omp_get_thread_num(), 3);
+            }
         }
+        std::cout << std::format("[{}]: parallel region\n", omp_get_thread_num());
     }
+
 }
 
 int main()
 {
-    int n, k;
-
-    if (!read_var(n, "n")) exit(-1);
+    int k;
     if (!read_var(k, "k")) exit(-1);
 
-
-    int **a, **b, **c;
-
-    a = matrix_init(n);
-    b = matrix_init(n);
-    c = matrix_init(n);
-
-    if (!read_matrix(a, n) || !read_matrix(b, n)) exit(-1);
-
-    multiply(a, b, c, n, k);
-
-    print_matrix(c, n);
-
-    matrix_free(a,n);
-    matrix_free(b, n);
-    matrix_free(c, n);
+    sections(k);
 
     return 0;
 }
