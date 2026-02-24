b408f19fb677ab394d67cc06b57d3a3d8b9a9a47
Canned-pineapple-8 <nastentsiya.filimonova@mail.ru>
2026-02-24 14:11:35 +0300

    Refactor code

--- OpenMpPractice.cpp
+++ OpenMpPractice.cpp
@@ -2,6 +2,7 @@
 #include <omp.h>
 #include <format>
 #include <string>
+#include <vector>
 
 template<typename T>
 int read_value(T& val)
@@ -67,6 +68,18 @@ int read_matrix(double**a, int n)
     return 1;
 }
 
+void fill_matrix(double** a, int n, double filler_value)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            a[i][j] = filler_value;
+        }
+    }
+    std::cout << std::endl;
+}
+
 void print_matrix(double** a, int n)
 {
     for (int i = 0; i < n; ++i)
@@ -79,9 +92,12 @@ void print_matrix(double** a, int n)
     }
 }
 
-void multiply(double** a, double** b, double** c, int n, int threads_num)
+void multiply(double** a, double** b, double** c, int n, int threads_num, double& exec_time)
 {
-#pragma omp parallel for num_threads(threads_num) schedule(guided)
+    double start, end;
+   
+    start = omp_get_wtime();
+#pragma omp parallel for num_threads(threads_num) schedule(static)
     for (int i = 0; i < n; ++i)
     {
         for (int j = 0; j < n; ++j)
@@ -94,31 +110,39 @@ void multiply(double** a, double** b, double** c, int n, int threads_num)
             c[i][j] = sum;
         }
     }
+    end = omp_get_wtime();
+
+    exec_time = end - start;
 }
 
 int main()
 {
-    int n, k;
+    std::vector<int> threads_num = { 1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 36 };
+    std::vector<int> iterations = { 10, 1000 };
 
-    if (!read_var(n, "n")) exit(-1);
-    if (!read_var(k, "k")) exit(-1);
+    double exec_time;
 
+    for (size_t i_iter = 0; i_iter < iterations.size(); ++i_iter)
+    {
+        double** a = matrix_init(iterations[i_iter]);
+        double** b = matrix_init(iterations[i_iter]);
+        double** c = matrix_init(iterations[i_iter]);
 
-    double **a, ** b, ** c;
+        fill_matrix(a, iterations[i_iter], 10);
+        fill_matrix(b, iterations[i_iter], 10);
 
-    a = matrix_init(n);
-    b = matrix_init(n);
-    c = matrix_init(n);
+        for (size_t i_num = 0; i_num < threads_num.size(); ++i_num)
+        {
+            multiply(a, b, c, iterations[i_iter], threads_num[i_num], exec_time);
+            std::cout << std::format("Threads num: {}, iterations: {}\n", threads_num[i_num], iterations[i_iter]);
+            std::cout << std::format("\tExec time: {:.10f}\n\n", exec_time);
+        }
 
-    if (!read_matrix(a, n) || !read_matrix(b, n)) exit(-1);
+        matrix_free(a, iterations[i_iter]);
+        matrix_free(b, iterations[i_iter]);
+        matrix_free(c, iterations[i_iter]);
+    }
 
-    multiply(a, b, c, n, k);
-
-    print_matrix(c, n);
-
-    matrix_free(a,n);
-    matrix_free(b, n);
-    matrix_free(c, n);
 
     return 0;
 }
