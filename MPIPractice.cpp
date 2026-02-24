a57bc2168609145d20b81cdafd5df6f33ffbf3b9
Canned-pineapple-8 <nastentsiya.filimonova@mail.ru>
2026-02-24 17:08:11 +0300

    Add task-16

--- MPIPractice.cpp
+++ MPIPractice.cpp
@@ -2,15 +2,34 @@
 #include <iostream>
 #include <format>
 
+void first_second(int rank)
+{
+    if (rank == 0)
+    {
+        int size;
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+        std::cout << std::format("{} processes\n", size);
+    }
+    else if (rank % 2 == 0)
+    {
+        std::cout << std::format("I am process {}: SECOND\n", rank);
+    }
+    else
+    {
+        std::cout << std::format("I am process {}: FIRST\n", rank);
+    }
+    return;
+}
+
 int main(int argc, char* argv[])
 {
     MPI_Init(&argc, &argv);
 
-    int rank, size;
+    int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    std::cout << std::format("I am {} process from {} processes!\n", rank, size);
+    first_second(rank);
 
     MPI_Finalize();
     return 0;
