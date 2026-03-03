75cd1573d2a652e2edde845e888630119f53dcc9
Canned-pineapple-8 <nastentsiya.filimonova@mail.ru>
2026-03-03 12:15:17 +0300

    Add MPI_Finalize() before returning

--- MPIPractice.cpp
+++ MPIPractice.cpp
@@ -53,7 +53,11 @@ int main(int argc, char* argv[])
     int size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (!check_processes_num(size)) return 0;
+    if (!check_processes_num(size))
+    {
+        MPI_Finalize();
+        return 0;
+    }
 
     master_slave(size);
 
