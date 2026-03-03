6d4548d9db3e90caae06a0de5f2624bafa703a98
Canned-pineapple-8 <nastentsiya.filimonova@mail.ru>
2026-03-03 12:12:49 +0300

    Add task-20 wrong version

--- MPIPractice.cpp
+++ MPIPractice.cpp
@@ -3,19 +3,37 @@
 #include <format>
 #include <cstring>
 
-void send_message(const char * msg, int len, int recipient)
+int check_processes_num(int current_num, int min_num = 2)
 {
-    MPI_Send(&len, 1, MPI_INT, recipient, 0, MPI_COMM_WORLD);
-    MPI_Send(msg, len, MPI_CHAR, recipient, 1, MPI_COMM_WORLD);
+    if (current_num < min_num)
+    {
+        std::cout << std::format("Not enough processes ({}).", current_num);
+        return 0;
+    }
+
+    return 1;
 }
 
-char* receive_message(int sender)
+
+void send_message(const char * msg, int len, int recipient, MPI_Request* requests)
+{
+    MPI_Isend(&len, 1, MPI_INT, recipient, 0, MPI_COMM_WORLD, requests);
+    MPI_Isend(msg, len, MPI_CHAR, recipient, 1, MPI_COMM_WORLD, requests + 1);
+
+    MPI_Wait(requests, MPI_STATUS_IGNORE);
+    MPI_Wait(requests + 1, MPI_STATUS_IGNORE);
+}
+
+char* receive_message(int sender, MPI_Request* requests)
 {
     int len;
-    MPI_Recv(&len, 1, MPI_INT, sender, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Irecv(&len, 1, MPI_INT, sender, 0, MPI_COMM_WORLD, requests);
 
     char* buffer = new char[len];
-    MPI_Recv(buffer, len, MPI_CHAR, sender, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Irecv(buffer, len, MPI_CHAR, sender, 1, MPI_COMM_WORLD, requests + 1);
+
+    MPI_Wait(requests, MPI_STATUS_IGNORE);
+    MPI_Wait(requests + 1, MPI_STATUS_IGNORE);
 
     return buffer;
 }
@@ -27,25 +45,23 @@ int main(int argc, char* argv[])
     int size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (size < 2)
-    {
-        std::cout << std::format("Not enough processes ({}).", size);
-        return 0;
-    }
+    if (!check_processes_num(size)) return 0;
 
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    MPI_Request* reqs = new MPI_Request[2];
 
     if (rank == 0)
     {
         const char* message = "Hello from process 0!";
         int len = std::strlen(message);
 
-        send_message(message, len + 1, 1);
+        send_message(message, len + 1, 1, reqs);
     }
     else if (rank == 1)
     {
-        char* message = receive_message(0);
+        char* message = receive_message(0, reqs);
         std::cout << message << std::endl;
 
         delete[] message;
